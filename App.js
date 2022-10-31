a732cab3ab839e5fc4fa9a28705c5479a045c16b
Sixtus Anyanwu <sikky606@gmail.com>
2022-10-31 11:37:07 +0100

    initial setup

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableHighlight, TouchableNativeFeedback} from 'react-native';
 
 export default function App() {
+  let x;
+  const handlePress = () => console.log("Text Clicked")
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Hello World</Text>
+      <Text numberOfLines={1} onPress = {handlePress}>Hello World. How to make a text even longer for it last. So this is a long text.
+        For the record.
+      </Text>
+      <TouchableHighlight onPress={() => console.log("Pressed")}>
+        <Image 
+        blurRadius = {20}
+        fadeDuration = {1000}
+        source={{
+          uri: "https://thispersondoesnotexist.com/image"
+        }} style = {styles.img} />
+      </TouchableHighlight>
+      <TouchableNativeFeedback>
+        <View style = {{width: 300, height: 200, backgroundColor: "blue", display: "flex", 
+        flexDirection: "row", justifyContent: "center"}}>
+          <Text style = {{color: "white"}}> Some text</Text>
+        </View>
+      </TouchableNativeFeedback>
     </View>
   );
 }
@@ -17,4 +36,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  img: {
+    width: 90,
+    height: 60,
+    borderRadius: 40
+  }
 });
