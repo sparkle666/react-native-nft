9eacdf348df793b3f9951ec3ca68b5d5e297bd87
Sixtus Anyanwu <sikky606@gmail.com>
2022-11-02 20:59:54 +0100

    added image and styled it on details page

--- screens/Details.js
+++ screens/Details.js
@@ -1,9 +1,19 @@
 import { Text, View, Image, FlatList, StatusBar, SafeAreaView } from "react-native";
 import {COLORS, SIZES, FONTS, SHADOWS} from "../constants"
 import {SubInfo, RectButton, CircleButton, FocusedStatusBar, DetailsDesc, DetailsBid} from "../components"
+import React from "react";
 
+const DetailsHeader = ({data, navigation}) => (
+    <View style = {{width: '100%', height: 373}} >
+        <Image source={data.image} resizeMode = 'cover' 
+            style = {{width: "100%", height: '100%'}} 
+        />
+    </View>
+)
 const Details = ({route, navigation}) => {
-    console.log(route)
+    const {data} = route.params
+    console.log(data)
+    
     return (
         <SafeAreaView style= {{flex: 1}}>
             <FocusedStatusBar barStyle = "dark-content" backgroundColor = "transparent" 
@@ -21,6 +31,16 @@ const Details = ({route, navigation}) => {
             }}>
                 <RectButton minWidth={150} fontSize = {SIZES.large} {...SHADOWS.dark} />
             </View>
+            <FlatList data={data.bids} 
+            keyExtractor = {(item)=> item.id}
+            renderItem = {({item})=> <DetailsBid bid = {item} />}
+            showsVerticalScrollIndicator = {false}
+            contentContainerStyle = {{paddingBottom: SIZES.extraLarge * 3}}
+            ListHeaderComponent = {()=> 
+                <React.Fragment>
+                    <DetailsHeader data = {data} navigation = {navigation} />
+                </React.Fragment>} 
+            />
         </SafeAreaView>
     )
 }
